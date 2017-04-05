2f90dd48ee4119687780b8b582e115eb12db0017
daviddone <johnswdaviddone@163.com>
2017-04-05 11:35:18 +0800

    qingang website article

---
+++ marketing/qingang.py
@@ -0,0 +1,48 @@
+import requests
+import sys
+import io
+from bs4 import BeautifulSoup
+import json
+import datetime
+import time
+
+start_time = time.time()
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030') 
+page_get_url = 'http://www.iqingang.com/'
+reqheaders = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1'}
+r_page = requests.get(page_get_url,headers=reqheaders)
+page_soup = BeautifulSoup(r_page.content,"html.parser")
+page_no_text = page_soup.select('div.pagenavi a')[-1].get("href").split("=")[1]
+print("####"+str(page_no_text))
+page_no = int(page_no_text)
+create_time = datetime.datetime.now().strftime('%Y-%m-%d')
+file = open("秦刚营销列表"+create_time+".txt","w",encoding= 'utf-8')
+file_detail = open("秦刚营销详情"+create_time+".txt","w",encoding= 'utf-8')
+file_error = open("error"+create_time+".txt","w",encoding= 'utf-8')
+index = 1 
+for page_no in range(1,(page_no+1)):
+	site_url ='http://www.iqingang.com/?page='+str(page_no)
+	r = requests.get(site_url,headers=reqheaders)
+	soup = BeautifulSoup(r.content,"html.parser")
+	info_list = soup.select("div.post-list")
+	print(len(info_list))
+	for item in info_list:
+		info_title = item.select("div.post-title h2 a")[0].get_text().strip()
+		info_detail_href = item.select("div.post-title h2 a")[0].get("href").strip()
+		info_time = item.select("div.post-date small")[0].get_text()
+		file.writelines(str(index) + "," +info_title+ "," +info_time+"\n" +"\n")
+		print(info_title)
+		r_detail = requests.get(info_detail_href,headers=reqheaders)
+		soup_detail = BeautifulSoup(r_detail.content,"html.parser")
+		try:
+			detail_txt = soup_detail.select("dd.post-info")[0].get_text().strip()
+			info_time = soup_detail.select("div.post-title h4")[0].get_text()
+		except Exception as e:
+			print(str(info_detail_href)+"此条数据不完整")
+			file_error.writelines(str(e)+str(info_detail_href)+"此条数据不完整" + "\n")
+		file_detail.writelines(str(index) + "," +info_title+ "," +info_time+","+info_detail_href+","+detail_txt+"\n" +"\n")
+		index = index+1
+file.close()	 
+file_detail.close()
+file_error.close()
+print("run time s:"+str((time.time()-start_time)) )
