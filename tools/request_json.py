f9b6e8ba0a4ec976a33780e84e0abde74515176e
unknown <johnswdaviddone@163.com>
2017-07-25 08:53:08 +0800

    request传递json参数as body

---
+++ tools/request_json.py
@@ -0,0 +1,17 @@
+﻿#coding=utf-8
+import requests
+import json
+# 自定义菜单 access_token需调用接口获取
+access_token = "mFkOVsLuGc0Wg808S-jsxg-CM4nJoFvyMm5iacxdK05gdmhoXjRSQJogp5jqu7inC88DQKI9PecCGb_JsQvaTqpZkGguBJI1FBUs02iz40CCy21piH_wDP9GGqrCVnOlRXWdAEAZFE"
+body = {
+     "button":[
+     {
+          "type":"view",
+          "name":"菜单测试",
+          "url":"http://nc17545142.iok.la/money/"
+      }]
+ }
+url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token="+access_token
+data = json.dumps(body,ensure_ascii=False).encode('utf-8')
+r = requests.post(url, data)
+print(r.content)
