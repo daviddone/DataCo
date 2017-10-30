4be7beb74a84cf815be315a1870e2792f298e386
unknown <johnswdaviddone@163.com>
2017-10-30 18:41:07 +0800

    自动生成枚举类调整

--- script/make_java_code/make_enum.py
+++ script/make_java_code/make_enum.py
@@ -1,22 +1,24 @@
 f2 = open(r"enum.txt","w",encoding= 'utf-8')
 f3 = open(r"bean.txt","w",encoding= 'utf-8')
-f2.writelines("package com.boco.wangyou.mdt.entity;\n")
-f2.writelines("public enum IndexEsrvcc {\n")
+# f2.writelines("package com.boco.wangyou.mdt.entity;\n")
+# f2.writelines("public enum IndexEsrvcc {\n")
 f3.writelines("public class Test {\n")
 count = 0
-for line in open(r"source.txt",encoding= 'utf-8'):
-        if len(line.replace(" ","").strip()) <1:
-            continue
-        lists = line.strip().replace(" ","").split(",")
-        for item in lists:
-            new_item = item.upper() + "("+str(count)+")"+","
-            print(item.upper())
-            f3.writelines("\t private String "+item.lower().replace(" ","") +";" + "\n")
-            count = count + 1
-            if count == len(lists):
-                new_item  = new_item.replace(",",";")
-                print(new_item)
-            f2.write("\t"+new_item.replace(" ","")+"\n")
+lists = []
+for item in open(r"source.txt",encoding= 'utf-8'):
+    #print(len(line))
+    lists.append(item.strip().replace(" ","").upper())
+    print(item+"  000")
+print(len(lists))       
+for item in lists:
+    new_item = item.upper() + "("+str(count)+")"+","
+    #print(item.upper())
+    print("adf:"+item.upper())
+    count = count + 1
+    if count == len(lists):
+        new_item  = new_item.replace(",",";")
+        #print(new_item)
+    f2.write("\t"+new_item.replace(" ","")+"\n")
 
 common_line = '''
 	private int index;
@@ -32,7 +34,40 @@ common_line = '''
 	public void setIndex(int index) {
 		this.index = index;
 	}
-}
+
 '''
 f2.writelines(common_line)
-f3.writelines("}")
+f2.writelines("#################\n")
+for item in lists:
+    f2.writelines("\tprivate String "+item.lower().replace(" ","") +";" + "\n")
+f2.writelines("#################\n")
+for item in lists:
+	item = item.upper().replace(" ","")
+	line = "value.append(lineColumns[IndexUemrBaseEnum.%s"%item
+	newline = line+".getIndex()]).append(ConstantsUtil.VERTICAL_LINE);"
+	f2.writelines("\t"+newline + "\n")
+
+f2.writelines("#################\n")
+count = 0
+for item in lists:
+	line = item.lower().replace(" ","")
+	newline = line+"=lines[%s];"%count
+	count = count + 1
+	f2.writelines("\t"+newline + "\n")
+f2.writelines("#################\n")
+for item in lists:
+	line = item.lower().replace(" ","")
+	newline = "WritableUtils.writeString(out, this.%s);"%line
+	f2.writelines("\t"+newline + "\n")
+f2.writelines("#################\n")
+for item in lists:
+	line = item.lower().replace(" ","")
+	newline = "this.%s = WritableUtils.readString(in);"%line
+	f2.writelines("\t"+newline + "\n")
+f2.writelines("#################\n")
+f2.writelines("\t"+"MdtRoadBaseWritable outWritable = new MdtRoadBaseWritable();")
+for item in lists:
+	line = item.upper().replace(" ","")
+	newline = "outWritable.setTimestamp(lineColumns[IndexMdtUemrBaseEnum.%s.getIndex()]);"%line
+	f2.writelines("\t"+newline + "\n")
+f3.writelines("}"
