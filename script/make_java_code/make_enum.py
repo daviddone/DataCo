b57731d0e837a24daa8848d0abf050df186a87c5
unknown <johnswdaviddone@163.com>
2018-01-05 09:55:17 +0800

    枚举类吩咐

--- script/make_java_code/make_enum.py
+++ script/make_java_code/make_enum.py
@@ -5,7 +5,7 @@ f3 = open(r"bean.txt","w",encoding= 'utf-8')
 f3.writelines("public class Test {\n")
 count = 0
 lists = []
-for item in open(r"source.txt",encoding= 'utf-8'):
+for item in open(r"广西_base表.txt",encoding= 'utf-8'):
     #print(len(line))
     lists.append(item.strip().replace(" ","").upper())
     print(item+"  000")
@@ -41,10 +41,39 @@ f2.writelines("#################\n")
 for item in lists:
     f2.writelines("\tprivate String "+item.lower().replace(" ","") +";" + "\n")
 f2.writelines("#################\n")
+f2.writelines("#################\n")
+for item in lists:
+    f2.writelines("\t"+item.lower().replace(" ","") +"," + "\n")
+f2.writelines("#################\n")
+f2.writelines("create external table IF NOT EXISTS base ("+"\n")
+f2.writelines("create table IF NOT EXISTS base ("+"\n")
+for item in lists:
+    f2.writelines("\t"+item.lower().replace(" ","") +"  string," + "\n")
+f2.writelines(") partitioned by (DAY INT) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\|' STORED AS TEXTFILE LOCATION '${ParseInput}/base';"+"\n")
+f2.writelines(")partitioned by (DAY INT) row format delimited fields terminated by '\|';"+"\n")
+f2.writelines(") row format delimited fields terminated by '\|';"+"\n")
+f2.writelines("#################\n")
+for item in lists:
+    f2.writelines("\tpublic String "+item.lower().replace(" ","") +";" + "\n")
+f2.writelines("#################\n")
 for item in lists:
 	item = item.upper().replace(" ","")
 	line = "value.append(lineColumns[IndexUemrBaseEnum.%s"%item
 	newline = line+".getIndex()]).append(ConstantsUtil.VERTICAL_LINE);"
+	f2.writelines("\t"+newline + "\n")
+f2.writelines("#################\n")
+for item in lists:
+	item = item.upper().replace(" ","")
+	firstbig_line = item[0:1].upper()+item[1:len(item)].lower()
+	print("f:"+firstbig_line)
+	newline = "value.append(entity.get%s()).append(ConstantsUtil.VERTICAL_LINE);"%firstbig_line
+	f2.writelines("\t"+newline + "\n")
+f2.writelines("#################\n")
+
+for item in lists:
+	item = item.upper().replace(" ","")
+	line = "value.append(lineColumns[map.get(\"%s\")])"%item.lower()
+	newline = line+".append(ConstantsUtil.COMMA);"
 	f2.writelines("\t"+newline + "\n")
 
 f2.writelines("#################\n")
@@ -65,9 +94,39 @@ for item in lists:
 	newline = "this.%s = WritableUtils.readString(in);"%line
 	f2.writelines("\t"+newline + "\n")
 f2.writelines("#################\n")
-f2.writelines("\t"+"MdtRoadBaseWritable outWritable = new MdtRoadBaseWritable();")
+f2.writelines("\t"+"MdtRoadBaseWritable outWritable = new MdtRoadBaseWritable();\n")
 for item in lists:
 	line = item.upper().replace(" ","")
-	newline = "outWritable.setTimestamp(lineColumns[IndexMdtUemrBaseEnum.%s.getIndex()]);"%line
+	firstbig_line = line[0:1].upper()+line[1:len(line)].lower()
+	newline = "outWritable.set%s(lineColumns[IndexMdtUemrBaseEnum.%s.getIndex()]);"%(firstbig_line,line)
 	f2.writelines("\t"+newline + "\n")
-f3.writelines("}"
+f2.writelines("#################\n")
+for item in lists:
+	line = item.lower().replace(" ","")
+	firstbig_line = line[0:1].upper()+line[1:len(line)].lower()
+	newline = "entity.set%s(reduceValue.%s);"%(firstbig_line,line)
+	f2.writelines("\t"+newline + "\n")
+f2.writelines("#################\n")
+count = 0
+ncLine = ""
+ncLine_vertical = ""
+for item in lists:
+	line = item.upper().replace(" ","")
+	newline = "fileds[%s]"%count
+	ncLine = ncLine+newline + '+'
+	#ncLine_vertical = ncLine_vertical+'"'+line+':"+'+newline+ '+"|"+'
+	ncLine_vertical = ncLine_vertical+newline+ '+","+'
+	count = count +1
+f2.writelines("\t"+ncLine + "\n")
+f2.writelines("\t"+ncLine_vertical + "\n")
+f3.writelines("}")
+f2.writelines("#################\n")
+f2.writelines("\tpublic String getLine(){\n")
+f2.writelines("\tStringBuffer line = new StringBuffer();\n")
+for item in lists:
+	newitem = item.lower().replace(" ","")
+	newline = "line.append(%s).append(ConstantsUtil.VERTICAL_LINE);"%(newitem);	
+	f2.writelines("\t"+newline + "\n")
+f2.writelines("\tline.deleteCharAt(line.length() - 1);\n");
+f2.writelines("\treturn line.toString();\n")
+f2.writelines("}\n")
